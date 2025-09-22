77d3e437361591cce22918cfd65b1f4190c86d78
AnSeongSik-unreal <ahnbs135@naver.com>
2025-09-22 09:43:05 +0900

    3 배수합 계산 개선
    반복보다 연산이 더 시간이 오래 걸리므로 연산 제거

--- main.cpp
+++ main.cpp
@@ -10,12 +10,13 @@ int main()
 	for (int i = 1; i <= 100; i++)
 	{
 		OnetoHundreadSum += i;
-		if (i % 3 == 0)
-		{
-			TripleOnetoHundreadSum += i;
-		}
 	}
 	cout << "1 ~ 100까지 합 : " << OnetoHundreadSum << endl;
+
+	for (int i = 3; i <= 100; i += 3)
+	{
+		TripleOnetoHundreadSum += i;
+	}
 	cout << "1 ~ 100까지 존재하는 3의 배수 합 : " << TripleOnetoHundreadSum << endl;
 
 	return 0;
